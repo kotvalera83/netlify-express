575a0d6bd466ab8081fc63f0168d200d863294f7
Valerii Kot <kotvalera@gmail.com>
2020-01-31 05:45:44 -0800

    add sendgrid

--- express/server.js
+++ express/server.js
@@ -4,6 +4,25 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const sgMail = require('@sendgrid/mail');
+const allowDomain = ['https://null.jsbin.com'];
+const emailConfig = {
+  to: 'kotvalera@gmail.com',
+  from: 'kot@valerii.ml',
+  subject: 'This is the subject',
+};
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    if (allowDomain.indexOf(req.headers.origin) !== -1) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    }
+    next();
+});
+
+sgMail.setApiKey('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');
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -12,9 +31,16 @@ router.get('/', (req, res) => {
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.post('/', (req, res) => {
+  if(req.body.emailBody){
+    emailConfig.html = req.body.emailBody;
+    sgMail.send(emailConfig);
+    res.json({ message: 'send'});
+  }else{
+    res.json({ message: 'failed'});
+  }
+});
 
-app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
